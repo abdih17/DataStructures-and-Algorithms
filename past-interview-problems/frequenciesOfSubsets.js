d8a64f58d38986f26f2164d100704da29dda0130
Hawa Abdi <abdih17@hotmail.com>
2017-09-02 12:16:41 -0700

    found all the subsets of the array, need to find frequency now

---
+++ past-interview-problems/frequenciesOfSubsets.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// [2, 3, 4, 2, 3, 6, 2, 4, 3, 2, 4, 2, 3, 2, 3, 4]
+
+//2, 3, 4
+//3, 4, 2
+// 4, 2, 3, 6
+
+
+[2], 3, 4, 2, 3, 6, 2, 4, 3, 2, 4, 2, 3, 2, 3, 4
+
+2, [3], 4, 2, 3, 6, 2, 4, 3, 2, 4, 2, 3, 2, 3, 4
+
+var a = [2, 3, 4, 2, 3, 6, 2, 4, 3, 2, 4, 2, 3, 2, 3, 4];
+
+
+// { 2: true, 3: true, 4: true, 6: true }
+function frequenciesOfSubsets(a) {
+  var subsets = []; // array of arrays
+
+  for (var i = 0; i < a.length; i++) {
+    var startIndex = i;
+    var numbers = {};
+
+    for (var endIndex = startIndex + 1; endIndex < a.length; ++endIndex) {
+       var numberAtJ = a[j];
+
+       // number at J is repeating!
+       if (numbers[numberAtJ]) {
+         // endIndex points to the first index that's a repeating number
+         break;
+       } else {
+         numbers[numberAtJ] = true;
+       }
+    }
+
+    // our susbet is (startIndex, endIndex - 1);
+    var subset = a.slice(startIndex, endIndex);
+    subsests.push(subset);
+  }
+
+  // TODO: find repeating subsets
+}
+
+frequenciesOfSubsets(a)
